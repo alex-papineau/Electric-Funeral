c16036711584cccb536a2b570b7b9925a37ee4d5
alex-papineau <alex.papineau02@gmail.com>
2024-08-29 23:03:46 -0500

    Testing out new Tailwind integration

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,12 @@ function App() {
 
   return (
     <>
-      <div className="">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-black h-screen m-0">
+        <div className="flex items-center justify-center h-96">
+          <img src="https://upload.wikimedia.org/wikipedia/en/4/48/Vaudevillevillain.jpg" className="object-center" alt="" />
+        </div>
+        <code>Ha Ha Ha!</code>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
