8d89d40faaf9cc2669250b2373aa9dacd10a643b
alex-papineau <alex.papineau02@gmail.com>
2024-08-31 16:52:21 -0500

    added missing full screen support

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-black h-screen m-0">
+      <div className="bg-black h-screen w-screen m-0">
         <div className="flex items-center justify-center h-96">
           <img draggable="false" src="https://upload.wikimedia.org/wikipedia/en/4/48/Vaudevillevillain.jpg" className="object-center" alt="" />
         </div>
