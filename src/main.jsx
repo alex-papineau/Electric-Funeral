997a9695b9580f3db9fe3f9ef6cda19f9a18d049
alex-papineau <alex.papineau02@gmail.com>
2024-09-02 12:49:27 -0500

    added vercel analytics support

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
