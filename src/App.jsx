da3f3b752bcf9c46a047accf4c8aaebbeb5de267
alex-papineau <alex.papineau02@gmail.com>
2024-09-02 18:56:33 -0500

    placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ function App() {
   return (
     <>
       <div className="bg-black h-screen w-screen pt-44">
-        <div className="cool-border m-auto">
+        {/* <div className="cool-border m-auto">
         <iframe className="mx-auto my-4" src="https://open.spotify.com/embed/track/6rkV9hz91Rr5iHnMRPZdEQ?utm_source=generator&theme=0" width="50%" height="352"></iframe>
         </div>
-        <p>oh yeah</p>
+        <p>oh yeah</p> */}
+        <h1 className='text-7xl text-white text-center'>Working on it <code>:)</code></h1>
+        <p className='text-black'>i don't know when this will be done</p>
       </div>
     </>
   )
